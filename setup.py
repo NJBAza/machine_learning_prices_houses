d1ad4d485a0499609e8bbde8ad2053989880f27b
NJBAza <njbuitragoa@gmail.com>
2024-08-12 04:15:37 -0500

    updating config

--- setup.py
+++ setup.py
@@ -11,21 +11,23 @@ from setuptools import (
 # Metadata of package
 NAME = "prediction_model"
 DESCRIPTION = "Building a Machine Learning model"
-URL = 'https://github.com/manifoldailearning'
-EMAIL = "njavierbuitrago@gmail.com"
-AUTHOR = 'Javier Buitrago'
-REQUIRES_PYTHON = '>=3.7.0'
-LICENSE="MIT"
+URL = "https://njbaza.github.io/"
+EMAIL = "njavierbuitrago@gmail.com, njbuitragoa@gmail.com"
+AUTHOR = "Javier Buitrago"
+REQUIRES_PYTHON = ">=3.7.0"
+LICENSE = "MIT"
 README = "README.md"
 
+
 # Get the list of packages to be installed
-def list_reqs(fname='requirements.txt'):
-    with io.open(os.path.join(pwd, fname), encoding='utf-8') as f:
+def list_reqs(fname="requirements.txt"):
+    with io.open(os.path.join(pwd, fname), encoding="utf-8") as f:
         return f.read().splitlines()
 
+
 try:
-    with io.open(os.path.join(pwd, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(pwd, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
@@ -34,32 +36,32 @@ except FileNotFoundError:
 ROOT_DIR = Path(__file__).resolve().parent
 PACKAGE_DIR = ROOT_DIR / NAME
 about = {}
-with open(PACKAGE_DIR / 'VERSION') as f:
+with open(PACKAGE_DIR / "VERSION") as f:
     _version = f.read().strip()
-    about['__version__'] = _version
+    about["__version__"] = _version
 
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
-    package_data={'prediction_model': ['VERSION']},
+    packages=find_packages(exclude=("tests",)),
+    package_data={"prediction_model": ["VERSION"]},
     install_requires=list_reqs(),
     extras_require={},
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
